359c84899d4e513ca212f0f8dba67657b0c52b87
defidrum <jordandrummond0@gmail.com>
2024-06-30 18:02:01 -0500

    pushing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Routes, Route } from 'react-router-dom';
 import Home from "../src/components/Home/Home"
 import Explore from 'components/Explore/Explore';
 import Profile from 'components/Profile/Profile';
+import Search from 'components/Search/Search';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
           <Route path="/" element={<Home/>}/>
           <Route path="/explore" element={<Explore/>}/>
           <Route path="/profile" element={<Profile/>}/>
+          <Route path="/search" element={<Search/>}/>
       </Routes>
   );
 }
